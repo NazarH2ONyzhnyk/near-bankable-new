f74ef51b307a25ce4f3bb0908cf292af0d3115fe
Nazar Nyzhnyk <nazarh2onyzhnyk@gmail.com>
2021-10-20 05:35:43 +0300

    project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+
+  theme: {
+    fontFamily: {
+      'body': ['"Segoe UI"'],
+    },
+    extend: {
+
+      colors: {
+
+        near: {
+          500: '#95E1FF',
+          600: '#95E1FF',
+          700: '#65A6D5',
+          800: '#3A4666',
+          900: '#1D2D2D',
+        },
+        yellow: {
+          500: '#FECB58',
+        }
+      },
+
+    },
+  },
+  variants: {
+    extend: {
+      scale: ['active'],
+    },
+  },
+  plugins: [],
+}
